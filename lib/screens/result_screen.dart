1f90977282d831cb90ce0dd07a9d0ac94554283a
takashi0602 <takashi@takashimelon.com>
2026-06-14 20:21:04 +0900

    feat: リザルト画面に勝敗パターンごとのキャラクター画像を表示する

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -15,12 +15,50 @@ class ResultScreen extends StatelessWidget {
     GameResult.allClear => 'assets/images/result_screen_background_1.png',
   };
 
+  /// キャラクター画像のリスト
+  List<String> get _characterImages => switch (result) {
+    GameResult.yugarockLose => [
+      'assets/images/spaholder_lose.png',
+      'assets/images/yugarock.png',
+    ],
+    GameResult.asyncronLose => [
+      'assets/images/yugarock_splashA.png',
+      'assets/images/spaholder.png',
+      'assets/images/yugarock_splashA.png',
+    ],
+    GameResult.allClear => [
+      'assets/images/yugarock_splashA.png',
+      'assets/images/spaholder.png',
+      'assets/images/asyncron_lose.png',
+    ],
+  };
+
+  List<Widget> _buildCharacterWidgets() {
+    if (_characterImages.length == 3) {
+      return [
+        Image.asset(_characterImages[0], width: 180, fit: BoxFit.contain),
+        Image.asset(_characterImages[1], width: 350, fit: BoxFit.contain),
+        Image.asset(_characterImages[2], width: 180, fit: BoxFit.contain),
+      ];
+    } else {
+      return _characterImages
+          .map((path) => Image.asset(path, width: 250, fit: BoxFit.contain))
+          .toList();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Positioned.fill(
           child: Image.asset(_backgroundImage, fit: BoxFit.cover),
+        ),
+        Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: _buildCharacterWidgets(),
+          ),
         ),
         Align(
           alignment: Alignment.bottomCenter,
